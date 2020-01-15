1a0ec34a6b6f991e54efb86f3095f760a583d420
klimt <klimt@ukr.net>
2020-01-15 17:37:32 +0200

    home task 15/01/2020

--- hw_0_7.py
+++ hw_0_7.py
@@ -1,6 +1,4 @@
-a = input('Введите длину стороны A треугольника, см:')
-b = input('Введите длину стороны B треугольника, см:')
-c = input('Введите длину стороны C треугольника, см:')
-d = (float(a)+float(b)+float(c))/2
-e = float(sqrt(float(d)*(float(d)-float(a))*(float(d)-float(b))*(float(d)-float(a))))
-print('Площадь реугольника=', e, 'см2')
+a = input('Введите радиус круга, см:')
+b = 3.1415
+c = float(a)*float(a)*float(b)
+print('Площадь круга=', c, 'см2')
