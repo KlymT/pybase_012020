b88ec5bac255bbc8d54283c96658687b4d24c548
klimt <klimt@ukr.net>
2020-01-15 17:57:29 +0200

    home task 15/01/2020

--- hw_0_6.py
+++ hw_0_6.py
@@ -2,6 +2,6 @@ a = input('Введите длину стороны A треугольника, 
 b = input('Введите длину стороны B треугольника, см:')
 c = input('Введите длину стороны C треугольника, см:')
 d = (float(a)+float(b)+float(c))/2
-e = float(sqrt(81))
-
-print('Площадь реугольника=', e, 'см2')
+e = float(d)*(float(d)-float(a))*(float(d)-float(b))*(float(d)-float(c))
+f = pow(float(e), .5)
+print('Площадь треугольника=', f, 'см2')
