6386a72cb0ed9b10722842f6db0ff7a47a65dbcb
klimt <klimt@ukr.net>
2020-01-28 17:58:27 +0200

    home task 2

--- hw_2_2.py
+++ hw_2_2.py
@@ -1,5 +1,4 @@
-a, b = int(input('Введите два значения - a, b; a = ')), int(input('b = '))
-def sum_of_natural_numbers (a, b):
+def sum_of_natural_numbers(a, b):
     if a <= b:
         d = (a + b)*(b-a+1)/2
 
@@ -8,8 +7,21 @@ def sum_of_natural_numbers (a, b):
 
     print('Сумма всех значений в заданном промежутке составляет', d)
 
+while True:
+    a, b = int(input('Введите два натуральных целых числа - a, b;\na = ')), int(input('b = '))
 
-sum_of_natural_numbers(a,b)
+    if a < 0 or b < 0:
+        print('Натуральные числа не могут принимать отрицательные значения')
+        continue
+    sum_of_natural_numbers(a, b)
+    print('Желаете повторить?\nD = Да\nN = Нет')
+    c = input()
+    if c == 'D':
+        continue
+    if c == 'N':
+        break
+
+
 
 
 
